ef9aa8e80fe9914b16f9b72344daa7243adc81e5
sergioxdev1 <50931929+sergioxdev1@users.noreply.github.com>
2023-02-23 22:20:58 -0700

    1 file modified

--- telephoneNumberValidation.js
+++ telephoneNumberValidation.js
@@ -20,3 +20,17 @@ function telephoneCheck(str) {
 }
 
 telephoneCheck("555-555-5555");
+
+/* EXPLANATION
+The function uses a regular expression to match valid US phone numbers. The regular expression ^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$ consists of the following parts:
+
+^ matches the start of the string
+(1\s?)? matches an optional "1" followed by an optional whitespace character
+(\(\d{3}\)|\d{3}) matches either a three-digit number surrounded by parentheses or a three-digit number without parentheses
+[\s-]? matches an optional whitespace character or hyphen
+\d{3} matches a three-digit number
+[\s-]? matches an optional whitespace character or hyphen
+\d{4} matches a four-digit number
+$ matches the end of the string
+The function uses the test() method of the regular expression object to check if the input string matches the regular expression. The test() method returns true if the input string matches the regular expression and false otherwise.
+*/
